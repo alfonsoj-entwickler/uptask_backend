ee102ade7c8d73054ce091641e43d716775d4811
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-03-03 19:11:37 +0100

    add middleware to validate project existence by ID

---
+++ src/middleware/project.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from "express";
+import Project, { InterfaceProject } from "../models/Project";
+
+declare global {
+  namespace Express {
+    interface Request {
+      project: InterfaceProject;
+    }
+  }
+}
+
+export async function validateProjectExists(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
+    if (!project) {
+      const error = new Error("Project not found");
+      return res.status(404).json({ error: error.message });
+    }
+    req.project = project;
+    next();
+  } catch (error) {
+    return res.status(404).json({
+      error: "There is a error in middleware - validate Project Exists",
+    });
+  }
+}
