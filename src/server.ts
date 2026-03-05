949930bd0f110bd23a84ca079ab4c7f72bcdf09a
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-03-05 21:01:44 +0100

    initialize server with Express, dotenv, and middleware setup

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
+import authRoutes from "./routes/authRoutes";
+import { corsConfig } from "./config/cors";
+import morgan from "morgan";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors(corsConfig));
+
+// Logging
+app.use(morgan("dev"));
+
+// Reading datas from forms
+app.use(express.json());
+
+// Routes
+app.use("/api/projects", projectRoutes);
+app.use("/api/auth", authRoutes);
+
+export default app;
